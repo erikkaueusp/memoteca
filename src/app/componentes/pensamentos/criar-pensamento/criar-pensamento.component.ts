a637868559f59dd7b40c46ad943c43eb6f1c5235
Erik Kaue <erikkaue@gmail.com>
2022-10-01 15:20:14 -0300

    add project

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PensamentoService } from '../pensamento.service';
+import { Pensamento } from '../pensamento/pensamento';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento = {
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1'
+  }
+
+  constructor(private service:PensamentoService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
+}
---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { PensamentoService } from '../pensamento.service';
+import { Pensamento } from '../pensamento/pensamento';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+  listaPensamentos: Pensamento[] = [];
+
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+  }
+
+}
